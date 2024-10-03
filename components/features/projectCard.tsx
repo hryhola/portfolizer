109baaec0421113aaf0a40b11069dd9210b7a477
Vladyslav Hryhola <v.hryhola@astoundcommerce.com>
2024-10-03 18:10:06 +0300

    Project card styling fixes

--- components/features/projectCard.tsx
+++ components/features/projectCard.tsx
@@ -23,28 +23,30 @@ export const ProjectCard: React.FC<ProjectCardProps> = (props) => {
         className='bg-cover bg-center flex justify-between min-h-32 border border-black rounded overflow-hidden drop-shadow-lg'
     >
         <div className='bg-white lg:min-w-56 p-5 border-r border-black grid grid-rows-[min-content]'>
-            {!props.published && <p className='uppercase font-italic tracking-[-2px] font-bold text-sm'>unpublished</p>}
-            <h3 className='text-2xl'>
-                <Link className='hover:underline' href={props.authorId + '/' + props.id}>
-                    {props.name}
-                </Link>
-            </h3>
-            
-            {typeof props.totalHours === 'number'
-                ? <p><MdAccessTime className='inline relative top-[-2px]' /> {props.totalHours} {props.totalHours > 1 ? 'hours' : 'hour'}</p>
-                : <></>}
-            {props.totalComplexity && <p>
-                {props.totalComplexity === 'High' && <MdSignalCellular4Bar className='inline relative top-[-2px]' />}
-                {props.totalComplexity === 'Medium' && <MdSignalCellular2Bar className='inline relative top-[-2px]' />}
-                {props.totalComplexity === 'Low' && <MdSignalCellular1Bar className='inline relative top-[-2px]' />}
-                &nbsp;
-                {props.totalComplexity} Complexity
-            </p>}
-            {props.client && <p className='self-end font-italic tracking-[-0.25em]'>{props.client}</p>}
+            <div className='space-y-2'>
+                {!props.published && <p className='uppercase font-italic tracking-[-2px] font-bold text-sm'>unpublished</p>}
+                <h3 className='text-2xl'>
+                    <Link className='hover:underline' href={props.authorId + '/' + props.id}>
+                        {props.name}
+                    </Link>
+                </h3>
+                
+                {typeof props.totalHours === 'number'
+                    ? <p><MdAccessTime className='inline relative top-[-2px]' /> {props.totalHours} {props.totalHours > 1 ? 'hours' : 'hour'}</p>
+                    : <></>}
+                {props.totalComplexity && <p>
+                    {props.totalComplexity === 'High' && <MdSignalCellular4Bar className='inline relative top-[-2px]' />}
+                    {props.totalComplexity === 'Medium' && <MdSignalCellular2Bar className='inline relative top-[-2px]' />}
+                    {props.totalComplexity === 'Low' && <MdSignalCellular1Bar className='inline relative top-[-2px]' />}
+                    &nbsp;
+                    {props.totalComplexity} Complexity
+                </p>}
+            </div>
+            <p className='self-end font-italic tracking-[-0.25em]'>{props.client}</p>
         </div>
         <div className='bg-gradient-to-l w-1/2 md:w-1/4 from-white grid justify-end text-right p-5'>
             {props.frameworks.length ? <ul className=''>
-                {props.frameworks.slice(0, 5).map(f => <li key={f}>{f}</li>)}
+                {props.frameworks.slice(0, 5).map(f => <li className='sm:max-xl:drop-shadow-[0_0_3px_white]' key={f}>{f}</li>)}
             </ul> : <></>}
             <p className={cn({ 'self-end': props.frameworks.length }, 'font-italic tracking-[-4px]')}>{props.dateCreated && props.dateCreated.getFullYear()}</p>
         </div>
